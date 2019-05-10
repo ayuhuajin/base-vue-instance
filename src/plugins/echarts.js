1a5e7a5e333982ff64dcaba44aebf28bc31f3817
wqh <wqh@nxin.com>
2019-05-10 14:34:57 +0800

    引入babel-plugin-equire 一个按需加载 echarts 模块的 babel 插件

--- src/plugins/echarts.js
+++ src/plugins/echarts.js
@@ -1,12 +1,20 @@
 import Vue from 'vue';
-// 完整引入
+//1. 完整引入
 // import echarts from 'echarts';
 
-// 按需引入
-import echarts from 'echarts/lib/echarts';
-import 'echarts/lib/chart/bar';
-import 'echarts/lib/component/legend';
-import 'echarts/lib/component/title';
+//2. 按需引入
+// import echarts from 'echarts/lib/echarts';
+// import 'echarts/lib/chart/bar';
+// import 'echarts/lib/component/legend';
+// import 'echarts/lib/component/title';
+
+//3. babel-plugin-equire - 一个按需加载 echarts 模块的 babel 插件
+const echarts = equire([
+  // 写上你需要的模块
+  'bar',
+  'legend',
+  'title'
+]);
 
 Vue.prototype.$echarts = echarts;
 export default echarts;
