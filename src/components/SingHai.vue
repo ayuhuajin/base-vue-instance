3463b8e5de7de0cf017355585342b5d0c7988391
wqh <wqh@nxin.com>
2019-05-17 16:57:54 +0800

    调整

--- src/components/SingHai.vue
+++ src/components/SingHai.vue
@@ -25,7 +25,7 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import LiveVideo from '@/components/LiveVideo.vue';
+import LiveVideo from '@/components/common/LiveVideo.vue';
 import About from '@/views/About.vue';
 import mixin from '@/assets/js/mixin.ts';
 import index from '@/store/modules/index.ts';
