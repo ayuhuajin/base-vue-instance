e63143371f6f74b8c93691f9caff2f3e5371b5ec
wqh <wqh@nxin.com>
2019-07-10 11:13:54 +0800

    性能优化 分析插件

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,35 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin; // vue分析工具
+function getProdExternals() {
+  return {
+    // echarts: 'echarts',
+    // 'element-ui': 'element-ui',
+    // axios: 'axios',
+    // 'chart.js': 'Chart',
+    // vue: 'Vue'
+  };
+}
 module.exports = {
   devServer: {
     port: 10086 //启动端口
   },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      process.env.NODE_ENV === 'production' ? getProdExternals() : {}; // 排除打包的插件
+      return {
+        plugins: [new BundleAnalyzerPlugin()] //性能分析
+      };
+    }
+  },
+
+  transpileDependencies: [
+    './node_modules/query-string/index.js',
+    './node_modules/strict-uri-encode/index.js',
+    './src/store/modules/index.ts'
+  ],
+  css: {
+    sourceMap: true //开启cssSourceMap
+  },
+  productionSourceMap: false // 生产环境打包 不产生 map文件
   // configureWebpack: c => {
   //   if (process.env.NODE_ENV == 'production') {
   //     c.externals = {
@@ -11,12 +39,4 @@ module.exports = {
   //     };
   //   }
   // },
-  transpileDependencies: [
-    './node_modules/query-string/index.js',
-    './node_modules/strict-uri-encode/index.js',
-    './src/store/modules/index.ts'
-  ],
-  css: {
-    sourceMap: true //开启cssSourceMap
-  }
 };
