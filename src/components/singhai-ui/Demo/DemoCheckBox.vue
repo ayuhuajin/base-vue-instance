d3684b3ee8ba420e108d568036e7d8f81172a542
wqh <wqh@nxin.com>
2020-01-09 17:33:04 +0800

    CheckBox  组件

--- src/components/singhai-ui/Demo/DemoCheckBox.vue
+++ src/components/singhai-ui/Demo/DemoCheckBox.vue
@@ -4,7 +4,24 @@
       多选按钮
     </template>
     <template v-slot:example>
-      <hai-check-box></hai-check-box>
+      <ul class="example">
+        <li>
+          <p>多选：{{ subjects }}</p>
+          <hai-check-box v-model="subjects" :label="'Vue'" :name="'subject'">Vue</hai-check-box>
+          <hai-check-box v-model="subjects" :label="'React'" :name="'subject'">React</hai-check-box>
+          <hai-check-box v-model="subjects" :label="'Angular'" :name="'subject'" :disabled="true"
+            >Angular</hai-check-box
+          >
+          <hai-check-box v-model="subjects" :label="'java'" :name="'subject'">java</hai-check-box>
+        </li>
+        <li>
+          <p>多选：{{ sports }}</p>
+          <hai-check-box v-model="sports" :label="'篮球'" :name="'sport'">篮球</hai-check-box>
+          <hai-check-box v-model="sports" :label="'网球'" :name="'sport'">网球</hai-check-box>
+          <hai-check-box v-model="sports" :label="'足球'" :name="'sport'" :disabled="true">足球</hai-check-box>
+          <hai-check-box v-model="sports" :label="'乒乓球'" :name="'sport'">乒乓球</hai-check-box>
+        </li>
+      </ul>
     </template>
   </demo-item>
 </template>
@@ -19,6 +36,8 @@ export default {
   data() {
     return {
       title: 'CheckBox 多选',
+      subjects: [],
+      sports: ['篮球'],
       params: [
         {
           name: 'cname',
@@ -27,6 +46,38 @@ export default {
           options: '---',
           default: 'normal',
           isMust: false
+        },
+        {
+          name: 'v-model',
+          desc: '复选框选中的值，必须是数组类型',
+          type: 'Array',
+          options: '[]',
+          default: '---',
+          isMust: false
+        },
+        {
+          name: 'label',
+          desc: '多选按钮候选值',
+          type: 'String, Number, Boolean',
+          options: '---',
+          default: '---',
+          isMust: true
+        },
+        {
+          name: 'name',
+          desc: '原生name属性，相同name属性的单选按钮视为一组',
+          type: 'String',
+          options: '---',
+          default: 'name',
+          isMust: false
+        },
+        {
+          name: 'disabled',
+          desc: '是否禁用',
+          type: 'Boolean',
+          options: '---',
+          default: 'false',
+          isMust: false
         }
       ]
     };
@@ -34,3 +85,8 @@ export default {
   methods: {}
 };
 </script>
+<style lang="scss" scoped>
+.ckeckBox-item {
+  margin-right: 10px;
+}
+</style>
--- src/components/singhai-ui/checkbox/HaiCheckBox.vue
+++ src/components/singhai-ui/checkbox/HaiCheckBox.vue
@@ -1,17 +1,118 @@
 <template>
-  <div class="ckeckBox">
-    多选
-  </div>
+  <label class="ckeckBox-item">
+    <input type="checkbox" v-model="checked" :value="label" :name="name" :disabled="disabled" />
+    <i></i>
+    <span><slot>多选框</slot></span>
+  </label>
 </template>
 
 <script>
 export default {
   name: 'HaiCheckBox',
+  props: {
+    value: {
+      type: Array,
+      default() {
+        return [];
+      }
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    label: {
+      type: [String, Number, Boolean],
+      required: true
+    },
+    name: {
+      type: String,
+      default: 'name'
+    },
+    text: {
+      type: String,
+      default: '多选框'
+    }
+  },
+
   data() {
     return {
       message: ''
     };
   },
+  computed: {
+    checked: {
+      get() {
+        return this.value;
+      },
+      set(newValue) {
+        this.$emit('input', newValue);
+      }
+    }
+  },
   methods: {}
 };
 </script>
+
+<style lang="scss" scoped>
+@import '../assets/common';
+.ckeckBox-item {
+  position: relative;
+  &:hover {
+    i {
+      border: 1px solid #bcbcbc;
+    }
+  }
+  input[type='checkbox'] {
+    position: absolute;
+    z-index: 1;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    opacity: 0;
+    &:checked {
+      & ~ i {
+        border: 1px solid #eee;
+        background: $mainColor;
+        &:after {
+          content: '';
+          box-sizing: content-box;
+          border: 1px solid #fff;
+          border-left: 0;
+          border-top: 0;
+          height: 7px;
+          left: 6px;
+          position: absolute;
+          top: 2px;
+          width: 3px;
+          transition: transform 0.15s ease-in 0.05s;
+          transform-origin: center;
+          transform: rotate(45deg) scaleY(1.2);
+        }
+      }
+    }
+    &:disabled {
+      cursor: not-allowed;
+      & ~ i {
+        background: #f8f4f4;
+      }
+      & ~ span {
+        color: #c0c4cc;
+      }
+    }
+  }
+  i {
+    position: relative;
+    width: 18px;
+    height: 18px;
+    margin-right: 6px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    transition: all 0.3s;
+    vertical-align: middle;
+  }
+  span {
+    vertical-align: middle;
+  }
+}
+</style>
