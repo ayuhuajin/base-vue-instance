523508681bb960bdf42abd8b77c696d45a956e8b
wqh <wqh@nxin.com>
2020-01-14 15:37:18 +0800

    富文本改markdown 编辑器

--- src/components/frondEnd/BlogDetail.vue
+++ src/components/frondEnd/BlogDetail.vue
@@ -9,7 +9,16 @@
       </div>
     </section>
     <section class="content">
-      <div v-highlight v-html="html_decode(blog[0].content)"></div>
+      <!-- markdown 编辑 -->
+      <mavon-editor
+        v-model="blog[0].content"
+        :subfield="false"
+        :boxShadow="false"
+        defaultOpen="preview"
+        :toolbarsFlag="false"
+      />
+      <!-- 代码高亮编辑 -->
+      <!-- <div v-highlight v-html="html_decode(blog[0].content)"></div> -->
     </section>
   </div>
 </template>
@@ -19,17 +28,57 @@ import Vue from 'vue';
 import blog from '@/store/modules/blog';
 import index from '@/store/modules/index';
 import minxin from '@/assets/js/mixin';
+import { mavonEditor } from 'mavon-editor';
+import 'mavon-editor/dist/css/index.css';
 export default Vue.extend({
   name: 'FBlogDetail',
   mixins: [minxin],
+  components: {
+    mavonEditor
+  },
   data() {
     return {
       id: this.$route.query.id,
+      toolbars: {
+        bold: true, // 粗体
+        italic: true, // 斜体
+        header: true, // 标题
+        underline: true, // 下划线
+        strikethrough: true, // 中划线
+        mark: true, // 标记
+        superscript: true, // 上角标
+        subscript: true, // 下角标
+        quote: true, // 引用
+        ol: true, // 有序列表
+        ul: true, // 无序列表
+        link: true, // 链接
+        imagelink: true, // 图片链接
+        code: true, // code
+        table: true, // 表格
+        fullscreen: true, // 全屏编辑
+        readmodel: true, // 沉浸式阅读
+        htmlcode: true, // 展示html源码
+        help: true, // 帮助
+        /* 1.3.5 */
+        undo: true, // 上一步
+        redo: true, // 下一步
+        trash: true, // 清空
+        save: true, // 保存（触发events中的save事件）
+        /* 1.4.2 */
+        navigation: true, // 导航目录
+        /* 2.1.8 */
+        alignleft: true, // 左对齐
+        aligncenter: true, // 居中
+        alignright: true, // 右对齐
+        /* 2.2.1 */
+        subfield: true, // 单双栏模式
+        preview: true // 预览
+      },
       blog: [
         {
-          content: '<p>3666565</p>',
-          time: '2019-12-27T03:54:31.461Z',
-          title: '3333',
+          content: '',
+          time: '',
+          title: '',
           author: '',
           category: '',
           __v: 0
@@ -54,6 +103,7 @@ export default Vue.extend({
         this.$set(this.blog[0], 'author', obj.author);
       }
     }
+    console.log(this.blog[0].content);
   },
   methods: {
     // 格式化时间
