e7fc09045a8347c1e02923b8d201527c8cf390eb
ayuhuajin <455493143@qq.com>
2020-05-04 15:21:27 +0800

    fix：试卷暂无数据

--- src/components/backEnd/testBank/BankDetail.vue
+++ src/components/backEnd/testBank/BankDetail.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="bank-detail">
     <div class="title"><img @click="back" class="back" src="../../../assets/images/icon_back.png" />试卷详情</div>
-    <div class="question-list" v-for="(item, index) in data" :key="index">
-      <div class="question">
-        <p>{{ item.questionTitle }}</p>
-        <span @click="editQuestion(item._id)">编辑</span>
+
+    <div v-if="data.length > 0">
+      <div class="question-list" v-for="(item, index) in data" :key="index">
+        <div class="question">
+          <p>{{ item.questionTitle }}</p>
+          <span class="edit" @click="editQuestion(item._id)">编辑</span>
+        </div>
+        <div class="options" v-for="(m, num) in item.options" :key="num">{{ m.name }}：{{ m.value }}</div>
       </div>
-      <div v-for="(m, num) in item.options" :key="num">{{ m.name }}：{{ m.value }}</div>
+      <!-- 弹窗 -->
+      <base-dialog :dialogInfo="dialogInfo" :showDialog="showDialog" @closeDialog="closeDialog">
+        <set-question :questionInfo="questionInfo" @handleSave="handleSave" @handleCancel="handleCancel"></set-question>
+      </base-dialog>
     </div>
-    <!-- 弹窗 -->
-    <base-dialog :dialogInfo="dialogInfo" :showDialog="showDialog" @closeDialog="closeDialog">
-      <set-question :questionInfo="questionInfo" @handleSave="handleSave" @handleCancel="handleCancel"></set-question>
-    </base-dialog>
+    <div class="nodata" v-if="nodata">试卷暂无数据</div>
   </div>
 </template>
 
@@ -28,6 +32,7 @@ export default Vue.extend({
   },
   data() {
     return {
+      nodata: false,
       value: '',
       // 弹窗设置
       dialogInfo: {
@@ -66,7 +71,12 @@ export default Vue.extend({
         pageNumber: 1,
         examId: this.$route.query.id
       });
-      this.data = result.data;
+      if (result.data && result.data.length > 0) {
+        this.data = result.data;
+        this.nodata = false;
+      } else {
+        this.nodata = true;
+      }
     },
     // 编辑试题
     async editQuestion(id) {
@@ -101,12 +111,31 @@ export default Vue.extend({
 .bank-detail {
   border-radius: 5px;
   background: white;
+  .nodata {
+    padding: 20px;
+    font-size: 20px;
+    height: 300px;
+    line-height: 300px;
+    color: #666;
+    text-align: center;
+  }
   .question-list {
     padding: 15px 20px;
+    border-bottom: 1px solid #eee;
     .question {
       display: flex;
       justify-content: space-between;
       margin-bottom: 15px;
+      font-size: 18px;
+      color: #333;
+    }
+    .edit {
+      cursor: pointer;
+      color: #409eff;
+    }
+    .options {
+      font-size: 16px;
+      color: #666;
     }
   }
   .back {
