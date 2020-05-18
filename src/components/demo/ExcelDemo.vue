0fda89ec1739d339f083d25547babe9b0428cb06
wangqinghai <455493143@qq.com>
2020-05-18 18:13:10 +0800

    fix:导入。。。

--- src/components/demo/ExcelDemo.vue
+++ src/components/demo/ExcelDemo.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="excel-demo">
 
-    <el-button @click="excelImport">导入</el-button>
+    <!-- <el-button @click="excelImport">导入</el-button> -->
+    <el-upload
+      class="upload-demo"
+      action=""
+      :on-change="handleChange"
+      :on-exceed="handleExceed"
+      :on-remove="handleRemove"
+      accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.ms-excel"
+      :auto-upload="false">
+      <el-button size="small" type="primary">点击上传</el-button>
+      <div slot="tip" class="el-upload__tip">只 能 上 传 xlsx / xls 文 件</div>
+    </el-upload>
     <el-button @click="excelExport">导出</el-button>
     <!-- 表格 -->
     <base-table :tableData="tableData">
@@ -74,9 +85,141 @@ export default Vue.extend({
         }
       ]
     },
+    //上传文件时处理方法
+      handleChange(file, fileList){
+          this.fileTemp = file.raw;
+          if(this.fileTemp){
+              if((this.fileTemp.type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+                  || (this.fileTemp.type == 'application/vnd.ms-excel')){
+                  // this.importfxx(this.fileTemp);
+                  this.excelImport();
+              } else {
+                  this.$message({
+                      type:'warning',
+                      message:'附件格式错误，请删除后重新上传！'
+                  })
+              }
+          } else {
+              this.$message({
+                  type:'warning',
+                  message:'请上传附件！'
+              })
+          }
+      },
+      //超出最大上传文件数量时的处理方法
+      handleExceed(){
+          this.$message({
+              type:'warning',
+              message:'超出最大上传文件数量的限制！'
+          })
+          return;
+      },
+      //移除文件的操作方法
+      handleRemove(file,fileList){
+          this.fileTemp = null
+      },
+      //  importfxx(obj) {
+      //       let _this = this;
+      //       let inputDOM = this.$refs.inputer;
+      //       // 通过DOM取文件数据
+
+      //       this.file = event.currentTarget.files[0];
+
+      //       var rABS = false; //是否将文件读取为二进制字符串
+      //       var f = this.file;
+
+      //       var reader = new FileReader();
+      //       //if (!FileReader.prototype.readAsBinaryString) {
+      //       FileReader.prototype.readAsBinaryString = function(f) {
+      //           var binary = "";
+      //           var rABS = false; //是否将文件读取为二进制字符串
+      //           var pt = this;
+      //           var wb; //读取完成的数据
+      //           var outdata;
+      //           var reader = new FileReader();
+      //           reader.onload = function(e) {
+      //               var bytes = new Uint8Array(reader.result);
+      //               var length = bytes.byteLength;
+      //               for (var i = 0; i < length; i++) {
+      //                   binary += String.fromCharCode(bytes[i]);
+      //               }
+      //               //此处引入，用于解析excel
+      //               var XLSX = require("xlsx");
+      //               if (rABS) {
+      //                   wb = XLSX.read(btoa(fixdata(binary)), {
+      //                   //手动转化
+      //                   type: "base64"
+      //                   });
+      //               } else {
+      //                   wb = XLSX.read(binary, {
+      //                   type: "binary"
+      //                   });
+      //               }
+      //               outdata = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]]);
+      //               //outdata就是读取的数据（不包含标题行即表头，表头会作为对象的下标）
+      //               //此处可对数据进行处理
+      //               // let arr = [];
+      //               // outdata.map(v => {
+      //               //     let obj = {}
+      //               //     obj.code = v['Code']
+      //               //     obj.name = v['Name']
+      //               //     obj.pro = v['province']
+      //               //     obj.cit = v['city']
+      //               //     obj.dis = v['district']
+      //               //     arr.push(obj)
+      //               // });
+      //               // _this.da=arr;
+      //               // _this.dalen=arr.length;
+      //               return arr;
+      //           };
+      //           reader.readAsArrayBuffer(f);
+      //       };
+      //       if (rABS) {
+      //           reader.readAsArrayBuffer(f);
+      //       } else {
+      //           reader.readAsBinaryString(f);
+      //       }
+      //   },
+
     // 导入
      excelImport() {
       console.log('导入');
+      let fileReader = new FileReader();
+      var file = event.currentTarget.files[0];
+      // 回调函数
+      fileReader.onload = ev => {
+        try {
+          let data = ev.target.result;
+          let workbook = XLSX.read(data, {
+            type: 'binary'
+          });
+          // excel读取出的数据
+          let excelData = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
+          // 将上面数据转换成 table需要的数据
+          console.log(excelData);
+
+          let arr = [];
+          excelData.forEach(item => {
+            let obj = {};
+            obj.questionTitle = item['题目'];
+            obj.subject = item['科目'];
+            obj.questionType = item['题型'];
+            obj.testPaper = item['所属试卷'];
+            obj.level = item['难度'];
+            obj.answer = item['答案'];
+            obj.questionDesc = item['答案解析'];
+            obj.time = item['发布日期'];
+            arr.push(obj);
+          });
+          this.tableData = [...arr];
+          console.log(this.tableData);
+        } catch (e) {
+          window.alert('文件类型不正确！');
+          return false;
+        }
+      };
+      // 读取文件 成功后执行上面的回调函数
+      fileReader.readAsBinaryString(file);
     },
     // 导出
     excelExport() {
