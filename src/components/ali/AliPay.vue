2d34579ce074685b31973e85417b9700ec6208ec
ayuhuajin <455493143@qq.com>
2020-05-24 13:11:19 +0800

    fix：阿里支付

---
+++ src/components/ali/AliPay.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="ali-pay">
+    测试阿里支付
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({});
+</script>
+
+<style lang="scss" scoped>
+.ali-pay {
+  color: #666;
+}
+</style>
---
+++ src/views/Ali.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="ali-pay">
+    <ali-pay></ali-pay>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import AliPay from '@/components/ali/AliPay';
+export default Vue.extend({
+  components: {
+    AliPay
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.ali-pay {
+  color: #666;
+}
+</style>
