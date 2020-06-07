0f5141ded95c902de2614a97494dd91aeae39b86
ayuhuajin <455493143@qq.com>
2020-06-08 00:13:10 +0800

    fix：回调地址

--- src/components/ali/PayCallback.vue
+++ src/components/ali/PayCallback.vue
@@ -6,7 +6,22 @@
 
 <script lang="js">
 import Vue from 'vue';
-export default Vue.extend({});
+export default Vue.extend({
+  mounted(){
+    let status = getUrlParam("trade_status");//3
+    if(status) {
+      window.location = "https://wulilang.com/demo";
+    }
+  },
+  methods:{
+    getUrlParam(name) {//封装方法
+      var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+      var r = window.location.search.substr(1).match(reg); //匹配目标参数
+      if (r != null) return unescape(r[2]);
+      return null; //返回参数值
+    }
+  }
+});
 </script>
 
 <style lang="scss" scoped>
