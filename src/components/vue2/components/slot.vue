741b59c00b0c3ac85f4672a112b5382576ca6a8b
wangqinghai <455493143@qq.com>
2022-02-22 19:01:26 +0800

    具名插槽，作用域插槽

--- src/components/vue2/components/slot.vue
+++ src/components/vue2/components/slot.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="slot">
-    slot实例
+    <slot>
+      默认的slot内容
+    </slot>
+    <slot name="header"></slot>
+    <slot name="content" :data="data"></slot>
+    <!-- 作用域插槽，简单的来说就是子组件提供给父组件的参数，该参数仅限于插槽中使用 -->
+    <slot name="test" :childValue="childValue"></slot>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 export default Vue.extend({
-  components: {}
+  name: 'SlotDemo',
+  components: {},
+  data() {
+    return {
+      childValue: '120',
+      data: [{ name: '李白', age: 20 }, { name: '杜甫', age: 25 }]
+    };
+  },
+  methods: {
+    handleClick(tab: any, event: any) {}
+  }
 });
 </script>
--- src/components/vue2/demo3.vue
+++ src/components/vue2/demo3.vue
@@ -1,6 +1,33 @@
 <template>
   <div class="demo3">
-    <slot-demo></slot-demo>
+    <!-- 1.插槽内容 -->
+    <slot-demo>
+      <div>的地方大师傅但是</div>
+    </slot-demo>
+    <!-- 2.插槽内容 不设置会使用默认的内容 -->
+    <slot-demo> </slot-demo>
+    <!-- 3.使用具名插槽 -->
+    <slot-demo>
+      <template v-slot="header">
+        234234
+      </template>
+    </slot-demo>
+    <!-- 4.作用域插槽，简单的来说就是子组件提供给父组件的参数，该参数仅限于插槽中使用 -->
+    <slot-demo>
+      <template v-slot:content="list">
+        <ul>
+          <li v-for="(item, index) in list" :key="index">
+            {{ list }}
+            884
+          </li>
+        </ul>
+      </template>
+    </slot-demo>
+    <slot-demo>
+      <template v-slot:test="childValue">
+        {{ childValue }}678
+      </template>
+    </slot-demo>
   </div>
 </template>
 
@@ -8,8 +35,17 @@
 import Vue from 'vue';
 import slotDemo from '@/components/vue2/components/slot.vue';
 export default Vue.extend({
+  name: 'Demo3',
   components: {
     slotDemo
+  },
+  data() {
+    return {
+      // list: [{}]
+    };
+  },
+  methods: {
+    handleClick(tab: any, event: any) {}
   }
 });
 </script>
