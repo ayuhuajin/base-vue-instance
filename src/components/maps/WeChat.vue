760d11b27988eefdc1ace88a0f9a56f236f9b80f
wangqinghai <455493143@qq.com>
2022-04-14 16:16:25 +0800

    fix：调整config.devtool

--- src/components/maps/WeChat.vue
+++ src/components/maps/WeChat.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="we-chat">
     <ul class="list">
-      <li v-for="(item,index) in chatList" :key="index" >
-        <span @click="addFriend(item)">{{item.wechat}}</span>
+      <li v-for="(item, index) in chatList" :key="index">
+        <span @click="addFriend(item)">{{ item.wechat }}</span>
         <a href="weixin://contacts/profile/sisixiaobai">sisixiaobai</a>
         <a href="weixin://addfriend/sisixiaobai">微江门KWON</a>
+        <a href="http://weixin.qq.com/r/ykzexmzEPzFArSil9xnY">kkk</a>
+        <a href="weixin://profile/gh_34bd692a9835">ggg</a>
+        <a href="weixin://qr/gh_34bd692a9835">jjj</a>
+        <a href="https://wxaurl.cn/ApS3VBKkA6a">lll</a>
       </li>
     </ul>
     <div @click="jumpAccount">视觉好物</div>
@@ -17,53 +21,52 @@ export default Vue.extend({
   name: 'BdMap',
   data() {
     return {
-      chatList:[
+      chatList: [
         {
-          name:'1111',
-          phone:'18859067063',
-          wechat:'18859067063'
+          name: '1111',
+          phone: '18859067063',
+          wechat: '18859067063'
         },
         {
-          name:'2222',
-          phone:'18859067063',
-          wechat:'12345'
+          name: '2222',
+          phone: '18859067063',
+          wechat: '12345'
         }
       ]
     };
   },
-  mounted() {
-  },
+  mounted() {},
   methods: {
     // 跳转微信公众号 关注页
-    jumpAccount(){
+    jumpAccount() {
       var ua = window.navigator.userAgent.toLowerCase();
-      console.log(ua,23234)
+      console.log(ua, 23234);
       //通过正则表达式匹配ua中是否含有MicroMessenger字符串
-      if(ua.match(/MicroMessenger/i) == 'micromessenger'){
-        window.location.href='https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzIxMDYyOTkxNQ==#wechat_redirect';
+      if (ua.match(/MicroMessenger/i) == 'micromessenger') {
+        window.location.href =
+          'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzIxMDYyOTkxNQ==#wechat_redirect';
         return true;
-      }else{
+      } else {
         this.$message.success('请在微信打开哦');
       }
-
     },
     // 加微信好友
-    addFriend(item){
-      console.log(item,7878);
-      this.copyUrl(item.wechat)
-      window.location.href='weixin://';
+    addFriend(item) {
+      console.log(item, 7878);
+      this.copyUrl(item.wechat);
+      window.location.href = 'weixin://';
       // alert(333)
       //  window.location.href='weixin://profile/gh_34bd692a9835';
       //  window.location.href='weixin://contacts/profile/sisixiaobai';
       //  window.location.href='weixin://qr/gh_34bd692a9835';
     },
     // 复制方法
-    copyUrl(text){
-      let input = document.createElement("input");
+    copyUrl(text) {
+      let input = document.createElement('input');
       input.value = text;
       document.body.appendChild(input);
       input.select();
-      input.setSelectionRange(0, input.value.length)
+      input.setSelectionRange(0, input.value.length);
 
       if (document.execCommand) {
         document.execCommand('Copy');
@@ -78,8 +81,8 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-.we-chat{
-  .list{
+.we-chat {
+  .list {
     height: 300px;
     overflow: auto;
     font-size: 18px;
